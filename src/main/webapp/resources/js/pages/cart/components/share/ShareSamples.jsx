a1af9f149720cd70c538c16828c651a998b26833
Josh Adam <josh.adam@canada.ca>
2021-08-12 09:19:10 -0500

    Fixed the way samples are loaded onto the page.  Uses same mechanism as the side panel.

--- src/main/webapp/resources/js/pages/cart/components/share/ShareSamples.jsx
+++ src/main/webapp/resources/js/pages/cart/components/share/ShareSamples.jsx
@@ -1,13 +1,20 @@
-import { List, Typography } from "antd";
+import { Button, List, Space, Typography } from "antd";
 import React from "react";
-import { useGetCartSamplesQuery } from "../../../../apis/cart/cart";
+import {
+  useGetCartQuery,
+  useRemoveSampleMutation,
+} from "../../../../apis/cart/cart";
 import { useGetSampleIdsForProjectQuery } from "../../../../apis/projects/project";
+import { IconRemove } from "../../../../components/icons/Icons";
+import { SampleDetailViewer } from "../../../../components/samples/SampleDetailViewer";
+import { grey3 } from "../../../../styles/colors";
 
 export function ShareSamples({ projectId }) {
   const [samples, setSamples] = React.useState();
   const [existing, setExisting] = React.useState();
+  const [removeSample] = useRemoveSampleMutation();
 
-  const { data: allSamples, isLoading } = useGetCartSamplesQuery();
+  const { data: allSamples, isLoading: isSamplesLoading } = useGetCartQuery();
   const { data: ids, isLoading: idsLoading } = useGetSampleIdsForProjectQuery(
     projectId,
     {
@@ -18,8 +25,10 @@ export function ShareSamples({ projectId }) {
   React.useEffect(() => {
     const updatedSamples = [];
     const updateExisting = [];
-    if (!isLoading && !idsLoading) {
-      allSamples.unlocked.forEach((sample) => {
+    if (!isSamplesLoading && !idsLoading) {
+      console.log("UPDATING - INSIDE");
+      console.log(allSamples);
+      allSamples.forEach((sample) => {
         // Find out if sample is in the selected project
         if (ids.includes(Number(sample.identifier))) {
           updateExisting.push(sample);
@@ -30,25 +39,60 @@ export function ShareSamples({ projectId }) {
     }
     setSamples(updatedSamples);
     setExisting(updateExisting);
-  }, [allSamples, ids, idsLoading, isLoading]);
+  }, [allSamples, ids, idsLoading, isSamplesLoading]);
 
   return (
     <div>
       <Typography.Title level={4}>Available Samples</Typography.Title>
-      <List
-        loading={isLoading}
-        bordered
-        dataSource={samples}
-        renderItem={(sample) => (
-          <List.Item
-            style={{
-              backgroundColor: sample.exists ? "orange" : "transparent",
-            }}
-          >
-            {sample.label}
-          </List.Item>
+      <Space direction="vertical" style={{ display: "block" }}>
+        <List
+          loading={isSamplesLoading}
+          bordered
+          dataSource={samples}
+          renderItem={(sample) => (
+            <List.Item
+              actions={[
+                <Button
+                  type="text"
+                  key="remove"
+                  icon={<IconRemove />}
+                  size="small"
+                  onClick={() => removeSample({ sampleId: sample.id })}
+                />,
+              ]}
+            >
+              <SampleDetailViewer
+                sampleId={sample.id}
+                removeSample={removeSample}
+              >
+                <Button size="small">{sample.label}</Button>
+              </SampleDetailViewer>
+            </List.Item>
+          )}
+        />
+        {existing?.length > 0 && (
+          <>
+            <Typography.Text>
+              Note: the following examples already exist in the destination
+              project and will not be shared
+            </Typography.Text>
+            <List
+              loading={isSamplesLoading}
+              bordered
+              dataSource={existing}
+              renderItem={(sample) => (
+                <List.Item
+                  style={{
+                    backgroundColor: grey3,
+                  }}
+                >
+                  {sample.label}
+                </List.Item>
+              )}
+            />
+          </>
         )}
-      />
+      </Space>
     </div>
   );
 }
