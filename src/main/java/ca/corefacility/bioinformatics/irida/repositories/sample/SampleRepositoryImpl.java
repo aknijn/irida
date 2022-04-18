ad9dd4d39fb536b2fcf1989184ec601b784f826e
aknijn <arnold.knijn@iss.it>
2022-04-18 21:45:18 +0200

    ISS

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/SampleRepositoryImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/SampleRepositoryImpl.java
@@ -58,12 +58,17 @@ public class SampleRepositoryImpl implements SampleRepositoryCustom {
 	 * {@inheritDoc}
 	 */
  	public String getClusterIdByCodes(Project project, List<String> sampleCodes) {
-		Query query = entityManager.createNativeQuery("SELECT mec.value FROM metadata_entry as mec " + 
+		String strSQL = "SELECT mec.value FROM metadata_entry as mec " + 
+			"INNER JOIN metadata_entry as mes on mec.sample_id = mes.sample_id " +
+			"WHERE mec.field_id = 7 AND mes.field_id = 8 AND mes.value IN (" + sampleCodes.stream().collect(Collectors.joining("','", "'", "'")) + ") " +
+			"ORDER BY ABS(length(mec.value) - length(replace(mec.value, '_', ''))-1.4)";
+		Query query = entityManager.createNativeQuery(strSQL);
+/**		Query query = entityManager.createNativeQuery("SELECT mec.value FROM metadata_entry as mec " + 
 			"INNER JOIN metadata_entry as mes on mec.sample_id = mes.sample_id " +
 			"WHERE mec.field_id = 7 AND mes.field_id = 8 AND mes.value IN (?) " +
-			"ORDER BY ABS(length(mec.value) - length(replace(mec.value, '_', ''))-1.4)");
-		query.setParameter(1, sampleCodes.stream().collect(Collectors.joining("','", "'", "'")));
-		logger.debug("query: " + query.toString());
+			"ORDER BY ABS(length(mec.value) - length(replace(mec.value, '_', ''))-1.4)"); **/
+		//query.setParameter(1, sampleCodes.stream().collect(Collectors.joining("','", "'", "'")));
+		logger.debug("query: " + strSQL);
 		String result = (String) query.getSingleResult();
 		return result;
 	}
