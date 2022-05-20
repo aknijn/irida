eb948266f9def9ac919c9771452bfaf98da4d168
aknijn <arnold.knijn@iss.it>
2022-05-20 15:53:54 +0200

    Update sample-edit.js

--- src/main/webapp/resources/js/pages/samples/sample-edit.js
+++ src/main/webapp/resources/js/pages/samples/sample-edit.js
@@ -21,18 +21,18 @@ $("#edit-form").submit(function () {
   }
 
   if ($("#organism").val() == "Coronavirus") {
-    const isolateCombo1 = $('input[name=isolate1]:checked').val()
-	if (typeof(isolateCombo1) == "undefined") { isolateCombo1 = "0"; }
-    const isolateCombo2 = $('input[name=isolate2]:checked').val()
-	if (typeof(isolateCombo2) == "undefined") { isolateCombo2 = "0"; }
-    const isolateCombo3 = $('input[name=isolate3]:checked').val()
-	if (typeof(isolateCombo3) == "undefined") { isolateCombo3 = "0"; }
-    const isolateCombo4 = $('input[name=isolate4]:checked').val()
-	if (typeof(isolateCombo4) == "undefined") { isolateCombo4 = "0"; }
-    const isolateCombo5 = $('input[name=isolate5]:checked').val()
-	if (typeof(isolateCombo5) == "undefined") { isolateCombo5 = "0"; }
-    const isolateCombo6 = $('input[name=isolate6]:checked').val()
-	if (typeof(isolateCombo6) == "undefined") { isolateCombo6 = "0"; }
+    const isolateCombo1 = "0";
+	if (typeof($('input[name=isolate1]:checked').val()) != "undefined") { isolateCombo1 = $('input[name=isolate1]:checked').val(); }
+    const isolateCombo2 = "0";
+	if (typeof($('input[name=isolate2]:checked').val()) != "undefined") { isolateCombo2 = $('input[name=isolate2]:checked').val(); }
+    const isolateCombo3 = "0";
+	if (typeof($('input[name=isolate3]:checked').val()) != "undefined") { isolateCombo3 = $('input[name=isolate3]:checked').val(); }
+    const isolateCombo4 = "0";
+	if (typeof($('input[name=isolate4]:checked').val()) != "undefined") { isolateCombo4 = $('input[name=isolate4]:checked').val(); }
+    const isolateCombo5 = "0";
+	if (typeof($('input[name=isolate5]:checked').val()) != "undefined") { isolateCombo5 = $('input[name=isolate5]:checked').val(); }
+    const isolateCombo6 = "0";
+	if (typeof($('input[name=isolate6]:checked').val()) != "undefined") { isolateCombo6 = $('input[name=isolate6]:checked').val(); }
     const isolateCombo = isolateCombo1+isolateCombo2+isolateCombo3+isolateCombo4+isolateCombo5+isolateCombo6;
     $("#isolate").val(isolateCombo);
   }
