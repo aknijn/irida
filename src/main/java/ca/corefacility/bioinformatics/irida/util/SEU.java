0d844cbd992034ccd30d00333cf15546b5c829c4
aknijn <arnold.knijn@iss.it>
2022-08-30 14:04:47 +0200

    Update SEU.java

--- src/main/java/ca/corefacility/bioinformatics/irida/util/SEU.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/util/SEU.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
 // import org.springframework.core.env.Environment;
 
 @Controller
